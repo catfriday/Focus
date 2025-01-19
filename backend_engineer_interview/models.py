88a6ecd7d0f005dd0aa232b177a715293960445c
Catrina Friday <cat.friday@gmail.com>
2025-01-19 17:35:24 -0500

    created Application model and migration files

--- backend_engineer_interview/models.py
+++ backend_engineer_interview/models.py
@@ -1,6 +1,7 @@
 import datetime
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
@@ -20,3 +21,16 @@ class Employee(Base):
     last_name: Mapped[str]
     date_of_birth: Mapped[datetime.date]
     secret: Mapped[str]
+    applications: Mapped[list] = relationship("Application", back_populates="employee")
+
+
+class Application(Base):
+
+    __tablename__: str = "application"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    leave_start_date: Mapped[datetime.date]
+    leave_end_date: Mapped[datetime.date]
+    employee_id: Mapped[int] = mapped_column(ForeignKey("employee.id"))
+
+    employee: Mapped[Employee] = relationship("Employee", back_populates="applications")
